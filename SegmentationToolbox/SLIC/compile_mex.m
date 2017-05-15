1511301eccb5e8392e7514d5ea44f62a553df7c8
soumya <dev.soumyabrata@gmail.com>
2017-05-15 12:09:14 +0800

    updated

--- SegmentationToolbox/SLIC/compile_mex.m
+++
@@ -1,2 +0,0 @@
-mex -g -largeArrayDims epfl_slico.cpp SLICO.cpp SLICO.h;
-mex -g -largeArrayDims epfl_slic.cpp SLIC.cpp SLIC.h;
--- SegmentationToolbox/SLIC/compile_mex2.m
+++
@@ -1,2 +0,0 @@
-mex -g -largeArrayDims epfl_slico.cpp SLICO.cpp SLICO.h;
-mex -g -largeArrayDims epfl_slic2.cpp SLIC.cpp SLIC.h;
